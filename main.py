5a5afe2c2916cded8d29716e74790fa47621e052
Mitko Tochev <mr.sir88@gmail.com>
2024-02-03 23:32:27 -0500

    seed before app

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from api.controllers.items_controller import ItemsController
 from api.database.configuration import Base, engine
 from api.database.models import User, Product
 
-app = FastAPI()
-
-items_controller = ItemsController()
-
-app.include_router(items_controller.router)
-
 Base.metadata.create_all(engine)
 
 # Seed some data
@@ -17,3 +11,13 @@ Product(id=1, name='Computer', is_active=True, price=1500.00)
 Product(id=2, name='Monitor', is_active=True, price=500.00)
 
 User(id=1, email='mitkomtoshev@gmail.com', hashed_password='Test1234ngjkdgndfgj', is_active=True)
+
+app = FastAPI()
+
+items_controller = ItemsController()
+
+app.include_router(items_controller.router)
+
+
+
+
